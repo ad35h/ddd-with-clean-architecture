27693cfd4958b08a70479213a1d12951bdf3c121
Adesh Fakooa <bhoovandev.fakooa@proximitybbdo.fr>
2021-02-15 16:03:34 +0400

    Added domain models

---
+++ src/Clean.Architecture.Core/Entities/Guestbook.cs
@@ -0,0 +1,19 @@
+﻿using Clean.Architecture.SharedKernel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clean.Architecture.Core.Entities
+{
+    public class Guestbook : BaseEntity
+    {
+        public string Name { get; set; }
+        public List<GuestbookEntry> Entries { get; set; }
+        public Guestbook()
+        {
+            Entries = new List<GuestbookEntry>();
+        }
+    }
+}
---
+++ src/Clean.Architecture.Core/Entities/GuestbookEntry.cs
@@ -0,0 +1,16 @@
+﻿using Clean.Architecture.SharedKernel;
+using System;
+
+namespace Clean.Architecture.Core.Entities
+{
+    public class GuestbookEntry : BaseEntity
+    {
+        public string Email { get; set; }
+        public string Message { get; set; }
+        public DateTimeOffset DateTimeCreated { get; set; }
+        public GuestbookEntry()
+        {
+            DateTimeCreated = DateTime.UtcNow;
+        }
+    }
+}
